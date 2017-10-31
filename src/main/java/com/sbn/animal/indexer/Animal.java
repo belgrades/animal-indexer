ee4f260a8954a5358ce8e07542c771d34f8ebd72
belgrades <fcremaldc@gmail.com>
2017-10-31 11:47:35 +0100

    Adding usage of abstract methods

--- src/main/java/com/sbn/animal/indexer/Animal.java
+++ src/main/java/com/sbn/animal/indexer/Animal.java
@@ -9,14 +9,27 @@ package com.sbn.animal.indexer;
  *
  * @author Fernando Crema
  */
-public class Animal {
-    public int age;
-    public String name;
-    public String description;
+public abstract class Animal {
+    int age;
+    String name, description;
     
     public Animal(int m_age, String m_name, String m_description){
         this.age = m_age;
+        this.name = m_name;
         this.description = m_description;
-        this.name = m_name; 
     }
+    
+    public int get_age(){
+        return this.age;
+    }
+    
+    public String get_name(){
+        return this.name;
+    }
+    
+    public String get_description(){
+        return this.description;
+    }
+     
+    abstract void feedMe();
 }
