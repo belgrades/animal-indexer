1bf46249f6ecd14bb181fcceef93e30062471830
belgrades <fcremaldc@gmail.com>
2017-10-31 01:18:49 +0100

    Adding base for animal.java

---
+++ src/main/java/com/sbn/animal/indexer/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sbn.animal.indexer;
+
+/**
+ *
+ * @author Fernando Crema
+ */
+public class Animal {
+    public int age;
+    public String name;
+    public String description;
+    
+    
+}
