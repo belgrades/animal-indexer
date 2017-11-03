c0d279f9386a58c5b293be985b1ef1eea8ef3016
belgrades <fcremaldc@gmail.com>
2017-11-03 16:24:39 +0100

    Changing names of packages

---
+++ src/main/java/com/sbn/earth/Carnivore.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sbn.earth;
+
+/**
+ *
+ * @author Fernando Crema
+ */
+public class Carnivore extends Animal{
+    public Carnivore(int m_age, String m_description, String m_name){
+        super(m_age, m_name, m_description);
+    }
+    
+    void feedMe(){
+    
+    }
+}
